2162e708a70b2e21e1e887848076115ea12bb489
Jonathan Garvin <jon@5valleys.com>
2012-06-08 23:54:46 -0600

    reenable s3 liason test

--- test/unit/s3_liason_test.rb
+++ test/unit/s3_liason_test.rb
@@ -4,9 +4,15 @@ require 'test_helper'
 class S3LiasonTest < ActiveSupport::TestCase
   
   setup :remove_s3_liason_roadblock
+  setup :load_s3_credentials
+  setup :create_test_bucket
   
-  test 'should write readable encrypted yaml file to s3' do
-    skip 'message' if true
+  teardown :delete_test_bucket
+  
+  test 'should write readable encrypted file to s3' do
+
+    skip 'S3 credentials for test bucket not provided.' unless Master.config['s3_credentials'].is_a?(Hash) and Master.config['s3_credentials'] != {}
+
     test_file_path = File.expand_path('../../test_folder/test_sub_folder/test_file_one.txt',  __FILE__)
     hash_key = Cryptographer.hash_file(test_file_path)
     assert_equal(0,S3Liason.send(:bucket).objects.count)
@@ -29,4 +35,22 @@ class S3LiasonTest < ActiveSupport::TestCase
     S3Liason.unstub(:write)
   end
   
+  def load_s3_credentials
+    FakeFS.deactivate!
+    real_config = YAML.load_file(Master::CONFIG_FILE) if File.exist?(Master::CONFIG_FILE)
+    FakeFS.activate!
+    
+    if real_config && real_config['s3_credentials']
+      Master.instance_variable_set(:@config, Master.config.merge({ 's3_credentials' => real_config['s3_credentials'] }))
+    end
+  end
+  
+  def create_test_bucket
+    Master.instance_variable_set(:@config, Master.config.merge({ 's3_bucket_name' => "cloud_encrypted_sync_unit_test_bucket_#{Digest::SHA1.hexdigest(rand.to_s)}" }))
+    S3Liason.send(:connection).buckets.create(Master.config['s3_bucket_name'])
+  end
+  
+  def delete_test_bucket
+    S3Liason.send(:bucket).delete! unless Master.config['s3_credentials'] == {} or !Master.config['s3_credentials'].is_a?(Hash)
+  end
 end
